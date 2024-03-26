2cf5a3bd995636cf6b2364c8d726b88a9b880576
Delgorithm <delgorithm@gmail.com>
2024-03-26 13:03:29 +0100

    Feature : Finished the props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import PokemonCard from "../components/PokemonCard";
 import "./App.css";
 
+
+const pokemonList = [
+	{
+		name: "bulbasaur",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+	},
+	{
+		name: "mew",
+	},
+];
+
 function App() {
 	return (
 		<>
-			<PokemonCard />
+			<PokemonCard  pokemon={pokemonList[0]}/>
 		</>
 	);
 }
