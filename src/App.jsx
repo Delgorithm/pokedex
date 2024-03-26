6e30f01b30f440fd4756022ba32dbb39e22712ba
Delgorithm <delgorithm@gmail.com>
2024-03-26 19:26:20 +0100

    Feature : Use of the state

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,56 @@
+import { useState } from "react";
 import PokemonCard from "../components/PokemonCard";
 import "./App.css";
 
 const pokemonList = [
 	{
-		name: "Bulbasaur",
+		name: "bulbasaur",
 		imgSrc:
-			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-	},
-	{
+		  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+	  },
+	  {
+		name: "charmander",
+		imgSrc:
+		  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+	  },
+	  {
+		name: "squirtle",
+		imgSrc:
+		  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+	  },
+	  {
+		name: "pikachu",
+		imgSrc:
+		  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+	  },
+	  {
 		name: "mew",
-	},
-];
+	  },
+	];
 
 function App() {
+	const [pokemonIndex, setPokemonIndex] = useState(0);
+
+	const handleDecrement = () => {
+		if (pokemonIndex > 0) {
+			setPokemonIndex(pokemonIndex - 1);
+		}
+	};
+
+	const handleIncrement = () => {
+		if (pokemonIndex < pokemonList.length - 1) {
+			setPokemonIndex(pokemonIndex + 1);
+		}
+	};
+
 
 	return (
 		<>
-			<PokemonCard pokemon={pokemonList[0]}/>
+			<PokemonCard pokemon={pokemonList[pokemonIndex]}/>
+			<article>
+				<button onClick={handleDecrement}>Précédent</button>
+				<button onClick={handleIncrement}>Suivant</button>
+			</article>
 		</>
 	);
 }
